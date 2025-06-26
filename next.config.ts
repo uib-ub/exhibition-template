b4ec9f5c75937990ab6f3a5cda457e235e27d878
Tarje Lavik <tarje.lavik@gmail.com>
2025-06-26 14:40:24 +0200

    Add GitHub Actions workflow for build and deployment to GitHub Pages, update Next.js configuration for static exports, base path, and image optimization settings, and add .nojekyll file to public directory.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,33 @@
 import nextra from 'nextra'
- 
+
 // Set up Nextra with its configuration
 const withNextra = nextra({
   // ... Add Nextra-specific options here
 })
- 
+
 // Export the final Next.js config with Nextra included
 export default withNextra({
-  // ... Add regular Next.js options here
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/exhibition-template",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 })
